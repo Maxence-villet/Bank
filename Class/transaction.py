f0b120fa5c7a110656d1e38c9cc027e5d80ffbac
jadmin <jossbessi460@gmail.com>
2025-10-08 14:21:56 +0200

    modify: transaction date state added

--- Class/transaction.py
+++ Class/transaction.py
@@ -9,14 +9,18 @@ class Transaction:
     amount: float
     uuid_transaction: str = field(default_factory=lambda: str(uuid4()))
     pending_at: datetime = field(default_factory=datetime.now)
-    completed_at: datetime = None
-    failed_at: datetime = None
+    completed_at: datetime | None = None
+    failed_at: datetime | None = None
+    cancelled_at: datetime | None = None
 
     def mark_completed(self):
-        object.__setattr__(self, 'completed_at', datetime.now())
+        object.__setattr__(self, "completed_at", datetime.now())
 
     def mark_failed(self):
-        object.__setattr__(self, 'failed_at', datetime.now())
+        object.__setattr__(self, "failed_at", datetime.now())
+
+    def mark_cancelled(self):
+        object.__setattr__(self, "cancelled_at", datetime.now())
 
 # Example usage:
 if __name__ == "__main__":
