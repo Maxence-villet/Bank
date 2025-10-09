20ca741a55d58b07a011ed8627e99b1138abae7d
AmandineGUISY <aguisy@yahoo.fr>
2025-10-09 12:07:56 +0200

    UPDATE: addition of the création of the database with models

--- api/load_app.py
+++ api/load_app.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from api.routers.user_router import router as user_router
 from api.routers.account_router import router as account_router
 from api.routers.current_account_router import router as current_account_router
 from api.routers.auth import router as auth_router
 from api.routers.beneficiary_router import router as beneficiary_router
 
+from db.database import Base, engine
+from models.user import User
+
 def load_app():
     app = FastAPI()
+
+    Base.metadata.create_all(bind=engine)
 
     app.include_router(user_router)
     app.include_router(account_router)
