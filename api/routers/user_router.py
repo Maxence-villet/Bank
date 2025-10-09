996799544853521a4ac34db0739805d86af7981f
Maxence-villet <maxence.villetschoumaker@gmail.com>
2025-10-09 17:00:25 +0200

    update: add secure routes

--- api/routers/user_router.py
+++ api/routers/user_router.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from api.crud.user_crud import register_user, get_users
+from utils.auth import get_current_user
+from fastapi import Depends
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -9,6 +11,6 @@ async def api_register_user(first_name: str, last_name: str, email: str, passwor
     return message
 
 @router.get("/")
-async def api_get_users():
+async def api_get_users(current_user: str = Depends(get_current_user)):
     users = get_users()
     return users
