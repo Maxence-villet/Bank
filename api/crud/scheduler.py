41125612eada8c643c6fc1a60be603c3b6057af9
jadmin <jossbessi460@gmail.com>
2025-10-09 17:36:43 +0200

    modify: optimisation where we check directly in the request pending and delay instead of iterating on all pending

--- api/crud/scheduler.py
+++ api/crud/scheduler.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from sqlmodel import Session, select
 from db.database import engine
 from models.transaction import TransactionModel
@@ -11,22 +11,31 @@ class TransactionScheduler:
         self.delay = delay  
 
     async def check_pending_transactions(self):
-        """Vérifie les transactions pending et déclenche leur finalisation."""
+   
+        now = datetime.now()
+        threshold = now - timedelta(seconds=self.delay)
+
         with Session(engine) as session:
-            stmt = select(TransactionModel).where(TransactionModel.status == TransactionStatus.pending)
+            stmt = (
+                select(TransactionModel)
+                .where(TransactionModel.status == TransactionStatus.pending)
+                .where(TransactionModel.pending_at <= threshold)
+            )
             pending_transactions = session.exec(stmt).all()
 
+            if not pending_transactions:
+                return  
+
             for tx_model in pending_transactions:
-                elapsed = (datetime.now() - tx_model.pending_at).total_seconds()
-                if elapsed >= self.delay:
-                    tx_entity = Transaction(
-                        sender_id=tx_model.sender_id,
-                        receiver_id=tx_model.receiver_id,
-                        amount=tx_model.amount,
-                        uuid_transaction=tx_model.uuid_transaction,
-                        pending_at=tx_model.pending_at,
-                        completed_at=tx_model.completed_at,
-                        failed_at=tx_model.failed_at,
-                        cancelled_at=tx_model.cancelled_at,
-                    )
-                    finalize_transaction(tx_entity, confirmed=True)
+                tx_entity = Transaction(
+                    sender_id=tx_model.sender_id,
+                    receiver_id=tx_model.receiver_id,
+                    amount=tx_model.amount,
+                    uuid_transaction=tx_model.uuid_transaction,
+                    pending_at=tx_model.pending_at,
+                    completed_at=tx_model.completed_at,
+                    failed_at=tx_model.failed_at,
+                    cancelled_at=tx_model.cancelled_at,
+                )
+
+                finalize_transaction(tx_entity, confirmed=True)
