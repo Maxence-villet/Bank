64c0c95addbed77cacded93753c5ead61653460f
AmandineGUISY <aguisy@yahoo.fr>
2025-11-26 22:52:16 +0100

    Making the myAccount Page, it's starting to looks like the figma asked

---
+++ front/src/components/Account/Account.tsx
@@ -0,0 +1,26 @@
+interface AccountType {
+    id: number,
+    name: string,
+    amount: number,
+    iban: string,
+}
+
+function Account(account: AccountType) {
+    return (
+        <>
+            <div className="text-left flex flex-col gap-4 background-white border-0 rounded-xl p-6 my-4 bg-white max-w-md">
+                <h2 className="text-xl">{account.name}</h2>
+                <div className="flex flex-column justify-between">
+                    <p className="font-bold">{account.amount}€</p>
+                    <p className="text-gray-400">{account.iban}...</p>
+                </div>
+                <div className="font-bold flex flex-row gap-4">
+                    <button className="border-2 rounded-md px-3 py-2">Transactions</button>
+                    <button className="border-2 rounded-md px-3 py-2">Cloturer</button>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Account;
