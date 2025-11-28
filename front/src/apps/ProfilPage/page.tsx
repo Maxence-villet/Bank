3a6668415e01ad927e6cbd2401d688929ac49441
AmandineGUISY <aguisy@yahoo.fr>
2025-11-28 12:08:40 +0100

    Making a component where you can modify the password

--- front/src/apps/ProfilPage/page.tsx
+++ front/src/apps/ProfilPage/page.tsx
@@ -1,7 +1,16 @@
+import ModiFyProfile from "../../components/Profil/ModifyProfile";
+
 function ProfilPage() {
     return (
         <>
-        <h1>TEST</h1>
+        <div className="pr-7 pl-16">
+            <div className="flex flex-row justify-between items-center mb-12">
+                <div className="text-left flex flex-col gap-4">
+                    <h1 className="text-5xl font-bold">Profil</h1>
+                </div>
+            </div>
+            <ModiFyProfile />
+        </div>
         </>
     )
 }
