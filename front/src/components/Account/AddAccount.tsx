7a16c92257d9412b29fa319ddeb9244a02bbb32a
AmandineGUISY <aguisy@yahoo.fr>
2026-01-20 11:18:09 +0100

    Mise en place d'un flag

--- front/src/components/Account/AddAccount.tsx
+++ front/src/components/Account/AddAccount.tsx
@@ -1,6 +1,8 @@
 import { useState, type SetStateAction } from "react";
 import { useAuth } from "../../contexts/AuthContext";
 import { useQueryClient } from "@tanstack/react-query";
+import { FeatureEnabled } from "flagster-react";
+import { FeatureDisabled } from "flagster-react";
 
 function addAccount() {
     const [isAdding, setIsAdding] = useState(false);
@@ -56,12 +58,24 @@ function addAccount() {
 
     return (
         <div>
-            <button onClick={() => resetAll()} className="border-2 border-black rounded-md font-bold text-xl px-5 py-3 flex flex-row gap-1 text-center">
-                Ajouter un compte
-                <svg width="24" height="24" viewBox="-4 -1 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M13 5C13 4.44772 12.5523 4 12 4C11.4477 4 11 4.44772 11 5V11H5C4.44772 11 4 11.4477 4 12C4 12.5523 4.44772 13 5 13H11V19C11 19.5523 11.4477 20 12 20C12.5523 20 13 19.5523 13 19V13H19C19.5523 13 20 12.5523 20 12C20 11.4477 19.5523 11 19 11H13V5Z" fill="#002222"/>
-                </svg>
-            </button>
+            <FeatureEnabled name="add_account">
+                <button onClick={() => resetAll()} className="border-2 border-black rounded-md font-bold text-xl px-5 py-3 flex flex-row gap-1 text-center">
+                    Ajouter un compte
+                    <svg width="24" height="24" viewBox="-4 -1 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M13 5C13 4.44772 12.5523 4 12 4C11.4477 4 11 4.44772 11 5V11H5C4.44772 11 4 11.4477 4 12C4 12.5523 4.44772 13 5 13H11V19C11 19.5523 11.4477 20 12 20C12.5523 20 13 19.5523 13 19V13H19C19.5523 13 20 12.5523 20 12C20 11.4477 19.5523 11 19 11H13V5Z" fill="#002222"/>
+                    </svg>
+                </button>
+            </FeatureEnabled>
+
+            <FeatureDisabled name="add_account">
+                <button className="border-2 border-black rounded-md font-bold text-xl px-5 py-3 flex flex-row gap-1 text-center disabled:opacity-50 cursor-not-allowed" disabled>
+                    Ajouter un compte
+                    <svg width="24" height="24" viewBox="-4 -1 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M13 5C13 4.44772 12.5523 4 12 4C11.4477 4 11 4.44772 11 5V11H5C4.44772 11 4 11.4477 4 12C4 12.5523 4.44772 13 5 13H11V19C11 19.5523 11.4477 20 12 20C12.5523 20 13 19.5523 13 19V13H19C19.5523 13 20 12.5523 20 12C20 11.4477 19.5523 11 19 11H13V5Z" fill="#002222"/>
+                    </svg>
+                </button>
+            </FeatureDisabled>
+
             {isAdding && (
                 <div className="px-7 py-8 border-0 rounded-2xl bg-white shadow-lg z-10 w-[700px] fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                     <h2 className="text-4xl font-bold mb-6 text-left">Ajouter un compte</h2>
--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,17 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createFlagster, FlagsterProvider } from "flagster-react";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'react-hot-toast'
 import './index.css';
 import App from './App';
   
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <Toaster position="bottom-right" />
-    </QueryClientProvider>
+    <FlagsterProvider
+      flagster={createFlagster()}
+      config={{
+          environment: import.meta.env.VITE_KEY_FLAG,
+      }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <Toaster position="bottom-right" />
+      </QueryClientProvider>
+    </FlagsterProvider>
   </StrictMode>,
 )
