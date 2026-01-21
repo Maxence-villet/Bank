ec90f839a219a9720db837f0b03efbd31abcf58d
AmandineGUISY <aguisy@yahoo.fr>
2026-01-21 13:06:47 +0100

    Modify the port of the branch

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ from api.load_app import load_app
 app = load_app()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
