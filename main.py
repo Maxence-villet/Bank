0ec8f7e8dabef6fc754233fc6800479e8529b67c
AmandineGUISY <aguisy@yahoo.fr>
2026-01-21 13:38:36 +0100

    update of the port for the deployment

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from api.load_app import load_app
 app = load_app()
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5432))
+    port = int(os.environ.get("PORT", 8000)) 
     uvicorn.run("main:app", host="0.0.0.0", port=port)
