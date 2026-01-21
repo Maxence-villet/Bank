c5edbedb05a996c10bcdf6977ca89f3a99918ecb
AmandineGUISY <aguisy@yahoo.fr>
2026-01-21 13:59:01 +0100

    Other tests, it's barrely annoying

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from api.load_app import load_app
 app = load_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000)) 
-    uvicorn.run("main:app", port=port)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
