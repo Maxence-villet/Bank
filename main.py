f828d4b16991dda3f53b01099d4e7e7eef24c983
AmandineGUISY <aguisy@yahoo.fr>
2026-01-21 13:48:01 +0100

    trying new things for the port

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ app = load_app()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000)) 
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run("main:app", port=port)
